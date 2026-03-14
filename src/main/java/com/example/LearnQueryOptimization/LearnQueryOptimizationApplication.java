041cd6456e3b1f84b7cb3b386f91722f9f561bb6
aryaaa40 <haekalkunz40@gmail.com>
2026-03-14 18:49:45 +0700

    feat: initial project springboot setup

---
+++ src/main/java/com/example/LearnQueryOptimization/LearnQueryOptimizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearnQueryOptimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnQueryOptimizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnQueryOptimizationApplication.class, args);
+	}
+
+}
